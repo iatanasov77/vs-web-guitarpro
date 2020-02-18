47d20bad6942082032fe3bea24a068b33b0cc6ec
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-02-18 04:25:13 +0000

    Commit

--- public/assets/player-controls.js
+++ public/assets/player-controls.js
@@ -5,12 +5,18 @@ function createTrack( scoreTrack )
 	<div class="track" data-track="${scoreTrack.Index}">
 		<div class="title" v-on:click="onClickTrackTitle"><i class="fa fa-eye-slash showHide"></i>${scoreTrack.Name}</div>
 		<div class="btn-group btn-group-xs">
+
+			<button type="button" class="btn btn-default solo" v-on:click="onClickTrackSolo">Solo</button>
+			<button type="button" class="btn btn-default mute" v-on:click="onClickTrackMute">Mute</button>
+
+			<!--
 			<button type="button" class="btn btn-default solo" v-on:click="onClickTrackSolo">
 				<svg class="icon"><use xlink:href="#icon-solo"></use></svg>
 			</button>
 			<button type="button" class="btn btn-default mute" v-on:click="onClickTrackMute">
 				<svg class="icon"><use xlink:href="#icon-mute"></use></svg>
 			</button>
+			-->
 		</div>
 		<input id="volume-${scoreTrack.Index}" type="text" />
 	</div>
