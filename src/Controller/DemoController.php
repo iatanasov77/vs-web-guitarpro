25dabb68075617ec8da84402bfe2914b16ed19ca
Ivan I. Atanasov <i.atanasov77@gmail.com>
2020-03-15 07:33:31 +0000

    Commit

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -7,10 +7,33 @@ use Symfony\Component\HttpFoundation\Response;
 class DemoController extends AbstractController
 {
     // https://docs.alphatab.net/master/assets/files/player.html
-    public function player( Request $request ): Response
-    {   
+    public function playerWorking( Request $request ): Response
+    {
+        $er             = $this->getDoctrine()->getRepository( 'App\Entity\Tablature' );
+        $oTablature     = $er->find( $request->attributes->get( 'id' ) );
+        
         return $this->render( 'demo_pages/tablature-player.html.twig', [
-            'tablature' => ''
+            'item'      => $oTablature
+        ]);
+    }
+    
+    public function playerLatest( Request $request ): Response
+    {
+        $er             = $this->getDoctrine()->getRepository( 'App\Entity\Tablature' );
+        $oTablature     = $er->find( $request->attributes->get( 'id' ) );
+        
+        return $this->render( 'demo_pages/tablature-player-latest.html.twig', [
+            'item'      => $oTablature
+        ]);
+    }
+    
+    public function playerEncore( Request $request ): Response
+    {
+        $er             = $this->getDoctrine()->getRepository( 'App\Entity\Tablature' );
+        $oTablature     = $er->find( $request->attributes->get( 'id' ) );
+        
+        return $this->render( 'demo_pages/tablature-player-encore.html.twig', [
+            'item'      => $oTablature
         ]);
     }
 }
