f4abfd724ef8acfacd350c8035f21f86a50f9820
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-05-09 06:52:52 +0000

    Begin Authentication Functionalities.

--- themes/WebGuitarPro_VueJs/assets/js/includes/fos_js_routes.js
+++ themes/WebGuitarPro_VueJs/assets/js/includes/fos_js_routes.js
@@ -1,5 +1,5 @@
 // Manual: https://github.com/FriendsOfSymfony/FOSJsRoutingBundle/blob/master/Resources/doc/usage.rst
-// bin/web-content-thief fos:js-routing:dump --format=json --target=public/shared_assets/js/fos_js_routes_application.json
+// bin/web-guitar-pro fos:js-routing:dump --format=json --target=public/shared_assets/js/fos_js_routes_application.json
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 const routes = require( '../../../../../public/shared_assets/js/fos_js_routes_application.json' );
 import Routing from '../../../../../vendor/friendsofsymfony/jsrouting-bundle/Resources/public/js/router.min.js';
