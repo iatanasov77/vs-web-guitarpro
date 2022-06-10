14fbf03772024ea20d9ec277506535d9dfde8ddf
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-06-10 12:02:57 +0000

    Hide Uncategorized Category If Empty.

--- themes/WebGuitarPro_VueJs/assets/js/pages/tablatures.js
+++ themes/WebGuitarPro_VueJs/assets/js/pages/tablatures.js
@@ -2,6 +2,10 @@ require( '@kanety/jquery-simple-tree-table/dist/jquery-simple-tree-table.js' );
 
 $( function()
 {
+    if ( uncategorizedTablatures <= 0 ) {
+        $( '#uncategorized-tablatures' ).hide();
+    }
+    
     $( '#tblTablatures' ).simpleTreeTable({
         opened: [0]
     });
