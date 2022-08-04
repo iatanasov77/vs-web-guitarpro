c3a0acd15e0c0899ec1fbc98a9bf882ee9bf71a7
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-08-04 17:04:05 +0000

    Add My Favorites Page.

--- src/Controller/WebGuitarPro/FavoritesController.php
+++ src/Controller/WebGuitarPro/FavoritesController.php
@@ -4,10 +4,36 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
 
 class FavoritesController extends AbstractController
 {
     use GlobalFormsTrait;
+    
+    /** @var TaxonomyInterface */
+    private $tabCategoriesTaxonomy;
+    
+    public function __construct(
+        EntityRepository $taxonomyRepository,
+        string $tabCategoriesTaxonomyCode
+    ) {
+        $this->tabCategoriesTaxonomy    = $taxonomyRepository->findByCode( $tabCategoriesTaxonomyCode );
+    }
+    
+    public function index( Request $request ): Response
+    {
+        $params = [
+            'tabForm'                       => $this->getTabForm()->createView(),
+            'tabCategoryForm'               => $this->getTabCategoryForm()->createView(),
+            'tabCategoriesTaxonomyId'       => $this->tabCategoriesTaxonomy->getId(),
+            'locales'                       => $this->getDoctrine()->getRepository( 'App\Entity\Application\Locale' )->findAll(),
+            'paidTablatureStoreServices'    => $this->getDoctrine()->getRepository( 'App\Entity\UsersSubscriptions\PayedServiceSubscriptionPeriod' )->findAll(),
+            
+            'tablatureUploadLimited'        => ! $this->checkTablatureLimit(),
+        ];
+        
+        return $this->render( 'Pages/Dashboard/favorites.html.twig', $params );
+    }
     
     public function addFavorite( $id, Request $request ): Response
     {
