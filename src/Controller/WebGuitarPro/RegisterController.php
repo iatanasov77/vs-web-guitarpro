dce447d7661573cbec8a543b387af536b9594e54
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-08-04 19:49:37 +0000

    Fix Register Form View.

---
+++ src/Controller/WebGuitarPro/RegisterController.php
@@ -0,0 +1,59 @@
+<?php namespace App\Controller\WebGuitarPro;
+
+use Vankosoft\UsersBundle\Controller\RegisterController as BaseRegisterController;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Sylius\Component\Resource\Repository\RepositoryInterface;
+use Sylius\Component\Resource\Factory\Factory;
+use Sylius\Bundle\ResourceBundle\Doctrine\ORM\EntityRepository;
+use Vankosoft\UsersBundle\Security\UserManager;
+
+class RegisterController extends BaseRegisterController
+{
+    use GlobalFormsTrait;
+    
+    /** @var TaxonomyInterface */
+    private $tabCategoriesTaxonomy;
+    
+    public function __construct(
+        UserManager $userManager,
+        RepositoryInterface $usersRepository,
+        Factory $usersFactory,
+        RepositoryInterface $userRolesRepository,
+        MailerInterface $mailer,
+        RepositoryInterface $pagesRepository,
+        array $parameters,
+        
+        EntityRepository $taxonomyRepository,
+        string $tabCategoriesTaxonomyCode
+    ) {
+        parent::__construct( $userManager, $usersRepository, $usersFactory, $userRolesRepository, $mailer, $pagesRepository, $parameters );
+
+        $this->tabCategoriesTaxonomy    = $taxonomyRepository->findByCode( $tabCategoriesTaxonomyCode );
+    }
+    
+    public function index( Request $request, MailerInterface $mailer ): Response
+    {
+        if ( $this->getUser() ) {
+            return $this->redirectToRoute( $this->params['defaultRedirect'] );
+        }
+        
+        if ( $request->isMethod( 'post' ) ) {
+            return parent::index( $request, $mailer );
+        }
+        
+        $params = [
+            'tabForm'                       => $this->getTabForm()->createView(),
+            'tabCategoryForm'               => $this->getTabCategoryForm()->createView(),
+            'tabCategoriesTaxonomyId'       => $this->tabCategoriesTaxonomy->getId(),
+            'locales'                       => $this->getDoctrine()->getRepository( 'App\Entity\Application\Locale' )->findAll(),
+            'paidTablatureStoreServices'    => $this->getDoctrine()->getRepository( 'App\Entity\UsersSubscriptions\PayedServiceSubscriptionPeriod' )->findAll(),
+            
+            'tablatureUploadLimited'        => ! $this->checkTablatureLimit(),
+        ];
+
+        return $this->render( '@VSUsers/Register/register.html.twig', array_merge( $params, $this->templateParams( $this->getForm() ) ) );
+    }
+}
