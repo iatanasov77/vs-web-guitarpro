ea16acd80e4d0b96e6e68f6060eabcbc03f6e4ef
Ivan I. Atanasov <i.atanasov77@gmail.com>
2022-08-28 15:54:52 +0000

    Fix All Issues in AdminPanel About VankosftApplication Library 1.9

--- src/Controller/AdminPanel/DefaultController.php
+++ src/Controller/AdminPanel/DefaultController.php
@@ -13,4 +13,20 @@ class DefaultController extends AbstractController
         
         return new RedirectResponse( $redirectUrl, 307 );
     }
+    
+    public function setLanguage( Request $request ): Response
+    {
+        $lang   = $request->attributes->get( 'lang' );
+        $request->getSession()->set( '_locale', $lang );
+        
+        return $this->redirect( $request->headers->get( 'referer' ) );
+    }
+    
+    public function setLocale( Request $request ): Response
+    {
+        $locale   = $request->attributes->get( 'locale' );
+        $request->getSession()->set( '_locale', $locale );
+        
+        return $this->redirect( $request->headers->get( 'referer' ) );
+    }
 }
