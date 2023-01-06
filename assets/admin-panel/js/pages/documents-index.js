53cf522910ef307dc655ab522f99cfafbb024aea
Ivan I. Atanasov <i.atanasov77@gmail.com>
2023-01-06 18:24:41 +0000

    Remove AdminPanel Assets From Project. Already Used From ApplicationBundle.

--- assets/admin-panel/js/pages/documents-index.js
+++
@@ -1,6 +0,0 @@
-require( '../includes/resource-delete.js' );
-
-$( function()
-{
-	
-});
--- assets/admin-panel/js/pages/users-roles-edit.js
+++
@@ -1,7 +0,0 @@
-require( 'jquery-easyui/css/easyui.css' );
-require( 'jquery-easyui/js/jquery.easyui.min.js' );
-
-$( function()
-{
-	
-});
