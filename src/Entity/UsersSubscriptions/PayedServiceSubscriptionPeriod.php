d36c0b83f2ca74396788e17686fa4a3a53c1787d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-01-15 06:02:12 +0000

    Improve Phing Build Config.

--- src/Entity/UsersSubscriptions/PayedServiceSubscriptionPeriod.php
+++ src/Entity/UsersSubscriptions/PayedServiceSubscriptionPeriod.php
@@ -2,33 +2,12 @@
 
 use Doctrine\ORM\Mapping as ORM;
 use Vankosoft\UsersSubscriptionsBundle\Model\PayedServiceSubscriptionPeriod as PayedServiceSubscriptionPeriodBase;
-use Vankosoft\PaymentBundle\Model\Interfaces\PayableObjectInterface;
-use Vankosoft\PaymentBundle\Model\Traits\PaidServiceSubscriptionTrait;
-
-use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * @ORM\Entity
  * @ORM\Table(name="VSUS_PayedServiceSubscriptionPeriods")
  */
-class PayedServiceSubscriptionPeriod extends PayedServiceSubscriptionPeriodBase implements PayableObjectInterface
+class PayedServiceSubscriptionPeriod extends PayedServiceSubscriptionPeriodBase
 {
-    use PaidServiceSubscriptionTrait;
     
-    public function __construct()
-    {
-        $this->orderItems   = new ArrayCollection();
-        
-        parent::__construct();
-    }
-    
-    public function getSubscriptionCode(): ?string
-    {
-        return $this->getPayedService()->getSubscriptionCode();
-    }
-    
-    public function getSubscriptionPriority(): ?int
-    {
-        return $this->getPayedService()->getSubscriptionPriority();
-    }
 }
