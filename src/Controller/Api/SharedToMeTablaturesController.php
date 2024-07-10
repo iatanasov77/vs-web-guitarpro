7e46a9d7ce497185d66b435235e93f6cdae7948d
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-07-10 15:18:51 +0000

    Get Shared To Me Tablatures From API.

---
+++ src/Controller/Api/SharedToMeTablaturesController.php
@@ -0,0 +1,23 @@
+<?php namespace App\Controller\Api;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpKernel\Attribute\AsController;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
+class SharedToMeTablaturesController extends AbstractController
+{
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+    
+    public function __construct( TokenStorageInterface $tokenStorage )
+    {
+        $this->tokenStorage = $tokenStorage;
+    }
+    
+    public function __invoke(): iterable
+    {
+        $user   = $this->tokenStorage->getToken()->getUser();
+        
+        return $user->getTargetedShares();
+    }
+}
