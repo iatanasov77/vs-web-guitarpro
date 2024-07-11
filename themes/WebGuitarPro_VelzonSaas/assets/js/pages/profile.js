0da1fb541bc3eab198c7acd6d39ed5b24e7e05e8
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-07-11 08:09:47 +0000

    Add Notifications Dropdown on Top Bar.

--- themes/WebGuitarPro_VelzonSaas/assets/js/pages/profile.js
+++ themes/WebGuitarPro_VelzonSaas/assets/js/pages/profile.js
@@ -17,8 +17,8 @@ $( function()
         var triggerEl   = document.querySelector( '#ProfileTabs a[data-bs-target="#subscriptions"]' );
         bootstrap.Tab.getInstance( triggerEl ).show();
     } else {
-        var triggerFirstTabEl = document.querySelector( '#ProfileTabs li:first-child a' )
-        bootstrap.Tab.getInstance(triggerFirstTabEl).show() // Select first tab
+        //var triggerFirstTabEl = document.querySelector( '#ProfileTabs li:first-child a' )
+        //bootstrap.Tab.getInstance(triggerFirstTabEl).show() // Select first tab
     }
 
     $( '.btnChoosePlan' ).on( 'click', function()
